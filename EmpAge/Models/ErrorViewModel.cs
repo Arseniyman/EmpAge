35ef8277f4e44511c785bea3cd93d95da288ed16
Arseniy <Arseniy-sports@mail.ru>
2020-04-05 14:34:19 +0300

    Init

---
+++ EmpAge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmpAge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
